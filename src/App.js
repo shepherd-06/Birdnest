fc139f96f5ea495de8dcf34bae3ff687fbe97e86
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-01-09 23:59:07 +0200

    added fetch function, added cors package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+async function fetch_data() {
+
+  // this code returns promise fulfilled. no error, just I cant log any data.
+
+  fetch('https://assignments.reaktor.com/birdnest/drones/', {
+    mode: 'no-cors',
+  })
+    .then((response) => response.text())
+    .then((textResponse) => {
+      // let response_doc = new DOMParser().parseFromString(textResponse, 'application/xml');
+      console.log('response is ', textResponse);
+      console.log('len ', textResponse.length);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  fetch('https://assignments.reaktor.com/birdnest/drones/', {
+    mode: 'no-cors',
+  })
+    .then((response) => {
+      console.log("on another love");
+      console.log(response.text());
+      return response.text();
+    });
+}
+
 function App() {
+  fetch_data();
   return (
     <div className="App">
       <header className="App-header">
